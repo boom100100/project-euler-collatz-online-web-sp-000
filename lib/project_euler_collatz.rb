c6d8fa26444b42bf1a587a1a6f268246d03546ab
boom100100 <bsd231@nyu.edu>
2020-09-21 17:01:37 -0400

    Done.

--- lib/project_euler_collatz.rb
+++ lib/project_euler_collatz.rb
@@ -1,14 +1,32 @@
 def even_next(n)
+  return n/2;
 end
 
 def odd_next(n)
+  return (3 * n) + 1;
 end
 
 def next_value(n)
+  return n % 2 === 0 ? even_next(n) : odd_next(n);
 end
 
 def collatz(n)
+  i = n
+  target = [i]
+  while i > 1
+    i = next_value(i)
+    target << i
+  end
+  target
 end
 
 def longest_collatz
+  i = 1
+  longest = -1
+  while i < 1000000
+    result_starter = collatz(i)[0]
+    longest = result_starter if result_starter > longest
+    i += 1
+  end
+  longest
 end
